e73f9ad1e626f1c6904faecfcdf9345026b127ed
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-23 21:17:47 -0300

    create routes initials

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return <div className="App">hello world</div>;
-};
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
