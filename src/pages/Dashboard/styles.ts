31ffe45701b6c1d83c6ac6afa2676db648bd7278
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-24 11:27:57 -0300

    handling errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,28 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
+
+const treme = keyframes`
+        0% {
+          margin-left: 0;
+        }
+        25% {
+          margin-left: 5px;
+        }
+        50% {
+          margin-left: 0;
+        }
+        75% {
+          margin-left: -5px;
+        }
+        100% {
+          margin-left: 0;
+        }
+
+    }`;
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,9 +33,15 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
+
+  ${props =>
+    props.hasError &&
+    css`
+      animation: ${treme} 0.2s;
+    `}
 
   display: flex;
 
@@ -23,6 +52,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -43,6 +80,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
