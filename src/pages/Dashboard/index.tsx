742c433411788a5de63b296f69bfdc5611191ca4
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-24 10:49:46 -0300

    searching and listing repository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,87 +1,68 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/53957541?s=460&u=ef6399a73af271da633fcca5c67cde3d570a1cda&v=4"
-            alt="Vinícius Mendite"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>viniciusmendite/devfit</strong>
-            <p>Mantenha sua saúde e seu corpo em forma</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/53957541?s=460&u=ef6399a73af271da633fcca5c67cde3d570a1cda&v=4"
-            alt="Vinícius Mendite"
-          />
-
-          <div>
-            <strong>viniciusmendite/devfit</strong>
-            <p>Mantenha sua saúde e seu corpo em forma</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/53957541?s=460&u=ef6399a73af271da633fcca5c67cde3d570a1cda&v=4"
-            alt="Vinícius Mendite"
-          />
-
-          <div>
-            <strong>viniciusmendite/devfit</strong>
-            <p>Mantenha sua saúde e seu corpo em forma</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/53957541?s=460&u=ef6399a73af271da633fcca5c67cde3d570a1cda&v=4"
-            alt="Vinícius Mendite"
-          />
-
-          <div>
-            <strong>viniciusmendite/devfit</strong>
-            <p>Mantenha sua saúde e seu corpo em forma</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/53957541?s=460&u=ef6399a73af271da633fcca5c67cde3d570a1cda&v=4"
-            alt="Vinícius Mendite"
-          />
-
-          <div>
-            <strong>viniciusmendite/devfit</strong>
-            <p>Mantenha sua saúde e seu corpo em forma</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
